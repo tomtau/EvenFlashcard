a18e97d0738c2c31d9bfbfa38ea5853907e83239
whiskee.chen <whiskee.chen@evenrealities.com>
2024-11-26 18:04:26 +0800

    feat(notification): Android add notification feature

--- lib/views/features_page.dart
+++ lib/views/features_page.dart
@@ -1,76 +1,72 @@
-import 'package:demo_ai_even/ble_manager.dart';
-import 'package:demo_ai_even/services/features_services.dart';
+// ignore_for_file: library_private_types_in_public_api
+
+import 'dart:io';
+
+import 'package:demo_ai_even/views/features/bmp_page.dart';
+import 'package:demo_ai_even/views/features/notification/notification_page.dart';
 import 'package:flutter/material.dart';
 
 class FeaturesPage extends StatefulWidget {
+  const FeaturesPage({super.key});
+
   @override
   _FeaturesPageState createState() => _FeaturesPageState();
 }
 
 class _FeaturesPageState extends State<FeaturesPage> {
-
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Features'),
-      ),
-      body: Padding(padding: EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 44), 
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: () async {
-                if (BleManager.get().isConnected == false) return;
-                print("${DateTime.now()} to show bmp1-----------");
-                FeaturesServices().sendBmp("assets/images/image_1.bmp");
-              },
-              child: Container(
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.5),
-                  borderRadius: BorderRadius.circular(5),
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Features'),
+        ),
+        body: Padding(
+          padding:
+              const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 44),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const BmpPage()),
+                  );
+                },
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  alignment: Alignment.center,
+                  child: const Text("BMP", style: TextStyle(fontSize: 16)),
                 ),
-                alignment: Alignment.center,
-                child: Text("BMP 1", style: TextStyle(fontSize: 16)),
               ),
-            ),
-            const SizedBox(height: 16),
-            GestureDetector(
-              onTap: () async {
-                if (BleManager.get().isConnected == false) return;
-                print("${DateTime.now()} to show bmp2-----------");
-                FeaturesServices().sendBmp("assets/images/image_2.bmp");
-              },
-              child: Container(
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.5),
-                  borderRadius: BorderRadius.circular(5),
+              if (Platform.isAndroid)
+                GestureDetector(
+                  onTap: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const NotificationPage()),
+                    );
+                  },
+                  child: Container(
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.only(top: 16),
+                    child: const Text(
+                      "Notification",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
                 ),
-                alignment: Alignment.center,
-                child: Text("BMP 2", style: TextStyle(fontSize: 16)),
-              ),
-            ),
-            const SizedBox(height: 16),
-            GestureDetector(
-              onTap: () async {
-                if (BleManager.get().isConnected == false) return;
-                FeaturesServices().exitBmp(); // todo
-              },
-              child: Container(
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.5),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                alignment: Alignment.center,
-                child: Text("Exit", style: TextStyle(fontSize: 16)),
-              ),
-            ),
-          ]
-        )
-      )
-    );
-  }       
+            ],
+          ),
+        ),
+      );
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,13 +1,17 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'dart:async';
+
 import 'package:demo_ai_even/ble_manager.dart';
 import 'package:demo_ai_even/services/evenai.dart';
 import 'package:demo_ai_even/views/even_list_page.dart';
 import 'package:demo_ai_even/views/features_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -15,27 +19,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Timer? scanTimer;
   bool isScanning = false;
- 
+
   @override
   void initState() {
     super.initState();
-
     BleManager.get().setMethodCallHandler();
     BleManager.get().startListening();
     BleManager.get().onStatusChanged = _refreshPage;
   }
 
-  void _refreshPage() {
-    setState(() { });
-  }
+  void _refreshPage() => setState(() {});
 
   Future<void> _startScan() async {
-    setState(() {
-      isScanning = true;
-    });
+    setState(() => isScanning = true);
     await BleManager.get().startScan();
     scanTimer?.cancel();
-    scanTimer = Timer(Duration(seconds: 15), () { // todo
+    scanTimer = Timer(15.seconds, () {
+      // todo
       _stopScan();
     });
   }
@@ -43,150 +43,147 @@ class _HomePageState extends State<HomePage> {
   Future<void> _stopScan() async {
     if (isScanning) {
       await BleManager.get().stopScan();
-      setState(() {
-        isScanning = false;
-      });
+      setState(() => isScanning = false);
     }
   }
 
-  Widget blePairedList() {
-    return  Expanded(
-      child: ListView.separated(
-        separatorBuilder: (context, index) => SizedBox(height: 5),
-        itemCount: BleManager.get().getPairedGlasses().length,
-        itemBuilder: (context, index) {
-          final glasses = BleManager.get().getPairedGlasses()[index];
-          return GestureDetector(
-            onTap: () async {
-              String channelNumber = glasses['channelNumber']!;
-              await BleManager.get().connectToGlasses("Pair_$channelNumber");
-            },
-            child: Container(
-              height: 72,
-              padding: EdgeInsets.only(left: 16, right: 16),
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Pair: ${glasses['channelNumber']}'),
-                      Text('Left: ${glasses['leftDeviceName']} \nRight: ${glasses['rightDeviceName']}'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Even AI Demo'),
-        actions: [
-          InkWell(
-            onTap: () {
-              print("To Features Page...");
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => FeaturesPage()),
-              );
-            },
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-            child: const Padding(
-              padding: EdgeInsets.only(
-                left: 16, top: 12, bottom: 14, right: 16),
-                child: Icon(Icons.menu))
-          ),
-        ]
-      ),
-      body: Padding(padding: EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 44), 
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            GestureDetector(
+  Widget blePairedList() => Expanded(
+        child: ListView.separated(
+          separatorBuilder: (context, index) => const SizedBox(height: 5),
+          itemCount: BleManager.get().getPairedGlasses().length,
+          itemBuilder: (context, index) {
+            final glasses = BleManager.get().getPairedGlasses()[index];
+            return GestureDetector(
               onTap: () async {
-                if (BleManager.get().getConnectionStatus() == 'Not connected') {
-                  _startScan();
-                }
+                String channelNumber = glasses['channelNumber']!;
+                await BleManager.get().connectToGlasses("Pair_$channelNumber");
+                _refreshPage();
               },
               child: Container(
-                height: 100,
+                height: 72,
+                padding: const EdgeInsets.only(left: 16, right: 16),
                 decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.5),
+                  color: Colors.white,
                   borderRadius: BorderRadius.circular(5),
                 ),
-                alignment: Alignment.center,
-                child: Text(BleManager.get().getConnectionStatus(), style: TextStyle(fontSize: 16)),
-              ),
-            ),
-            const SizedBox(height: 16),
-            if (BleManager.get().getConnectionStatus() == 'Not connected') 
-              blePairedList(),
-
-            if (BleManager.get().isConnected)
-              Expanded(
-                child: GestureDetector(
-                  onTap: () async {
-                    // todo
-                    print("To AI History List...");
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => EvenAIListPage()),
-                    );
-                  },
-                  child: Container(
-                    color: Colors.white.withOpacity(0.5),
-                    padding: EdgeInsets.all(16),
-                    alignment: Alignment.topCenter,
-                    child: SingleChildScrollView(
-                      child: StreamBuilder<String>(
-                        stream: EvenAI.textStream,
-                        initialData: "Press and hold left TouchBar to engage Even AI.",
-                        builder: (context, snapshot) {
-                          return Obx(() {
-                            return EvenAI.isEvenAISyncing.value 
-                              ? const SizedBox(
-                                    width: 50, 
-                                    height: 50, 
-                                    child: CircularProgressIndicator(), 
-                                  ) // Color(0xFFFEF991)
-                              : Text(
-                                  snapshot.data ?? "Loading...",
-                                  style: TextStyle(fontSize: 14, color: BleManager.get().isConnected ? Colors.black : Colors.grey.withOpacity(0.5)),
-                                  textAlign: TextAlign.center,
-                                );
-                              }
-                          );
-                        })
-                    )
-                  ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Pair: ${glasses['channelNumber']}'),
+                        Text(
+                            'Left: ${glasses['leftDeviceName']} \nRight: ${glasses['rightDeviceName']}'),
+                      ],
+                    ),
+                  ],
                 ),
               ),
+            );
+          },
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Even AI Demo'),
+          actions: [
+            InkWell(
+              onTap: () {
+                print("To Features Page...");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FeaturesPage()),
+                );
+              },
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              child: const Padding(
+                padding:
+                    EdgeInsets.only(left: 16, top: 12, bottom: 14, right: 16),
+                child: Icon(Icons.menu),
+              ),
+            ),
           ],
         ),
-      ),
-      /*
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          String sn = await Proto.getLegSn(Proto.lR());
-          print("sn----------------$sn"); 
-        },
-        child: Text("SN"),
-      ),*/
-    );
-  }
+        body: Padding(
+          padding:
+              const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 44),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () async {
+                  if (BleManager.get().getConnectionStatus() ==
+                      'Not connected') {
+                    _startScan();
+                  }
+                },
+                child: Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(BleManager.get().getConnectionStatus(),
+                      style: const TextStyle(fontSize: 16)),
+                ),
+              ),
+              const SizedBox(height: 16),
+              if (BleManager.get().getConnectionStatus() == 'Not connected')
+                blePairedList(),
+              if (BleManager.get().isConnected)
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () async {
+                      // todo
+                      print("To AI History List...");
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const EvenAIListPage(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      color: Colors.white,
+                      padding: const EdgeInsets.all(16),
+                      alignment: Alignment.topCenter,
+                      child: SingleChildScrollView(
+                        child: StreamBuilder<String>(
+                          stream: EvenAI.textStream,
+                          initialData:
+                              "Press and hold left TouchBar to engage Even AI.",
+                          builder: (context, snapshot) => Obx(
+                            () => EvenAI.isEvenAISyncing.value
+                                ? const SizedBox(
+                                    width: 50,
+                                    height: 50,
+                                    child: CircularProgressIndicator(),
+                                  ) // Color(0xFFFEF991)
+                                : Text(
+                                    snapshot.data ?? "Loading...",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: BleManager.get().isConnected
+                                            ? Colors.black
+                                            : Colors.grey.withOpacity(0.5)),
+                                    textAlign: TextAlign.center,
+                                  ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      );
 
   @override
   void dispose() {
