cfe83bd2cc43ca1d69a424874907c654c73c70b8
Hawk <edy@edydeMacBook-Pro-2.local>
2024-12-11 21:17:53 +0800

    add text sending function and demo page

--- lib/views/features_page.dart
+++ lib/views/features_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:demo_ai_even/views/features/bmp_page.dart';
 import 'package:demo_ai_even/views/features/notification/notification_page.dart';
+import 'package:demo_ai_even/views/features/text_page.dart';
 import 'package:flutter/material.dart';
 
 class FeaturesPage extends StatefulWidget {
@@ -58,6 +59,28 @@ class _FeaturesPageState extends State<FeaturesPage> {
                   margin: const EdgeInsets.only(top: 16),
                   child: const Text(
                     "Notification",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const TextPage()),
+                  );
+                },
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(top: 16),
+                  child: const Text(
+                    "Text",
                     style: TextStyle(fontSize: 16),
                   ),
                 ),
